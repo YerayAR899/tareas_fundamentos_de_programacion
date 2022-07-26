66dd5954ca3c4b33ffd5e6ad824a349740e4e817
Yeray A.R <yeray.anyo@xarxatecactiva.com>
2022-07-26 20:02:26 +0200

    fix

--- ejercicio4.js
+++ ejercicio4.js
@@ -2,8 +2,8 @@ import { findmean } from "./findMean.js";
 let array = [];
 
 for (let i=0; i<6000; i++){
-    let firstDice = Math.round(Math.random()*5+1);
-    let secondDice = Math.round(Math.random()*5+1);
+    let firstDice = Math.floor(Math.random()*5+1);
+    let secondDice = Math.floor(Math.random()*5+1);
     let total = firstDice+secondDice;
     array.push(total)
 }
